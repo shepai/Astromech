2aa984d3c9ca9f35ca25af673118b5f5a887597c
shepai <dextershepherd@hotmail.co.uk>
2021-02-19 13:53:47 +0000

    Create Python Class Overview.py

---
+++ Python Class Overview.py
@@ -0,0 +1,59 @@
+"""
+Astromech project
+
+Lead by D R Shepherd, aged 19
+Project Dev:
+*
+*
+*
+*
+*
+*
+*
+*
+
+This project is a hybrid intelligence project, using in-built useful features, as well
+as provide learning abilities with objects and interaction. This project too aims to experiment with artificial
+emotions and personality. 
+
+Plan to use a 3D printed chassis of a Star Wars Astromech (R6 unit)
+Using Arduino for control of hardware and situate a Ubuntu running computer
+for the main controller. This will be using a laptop battery to allow mobility.
+
+"""
+class emotions:
+    #track emotions and influence
+    #track emotions towards recognized people
+    pass
+class actuators:
+    #proide communication with motor systems
+    #communicate with projector
+    pass
+
+class inBuilt:
+    #provides all the inbuild functions
+    #calendar
+    #weather
+    #mathematical support
+    #wikipedia information
+    #verb control
+    pass
+
+class vision:
+    #Takes in vision from outside
+    #finds obstacles and edges
+    #locates objects
+    #locate wall
+    pass
+
+class map:
+    #Maps out areas it travels in
+    #Finds shorter ways through
+    pass
+
+
+class audio:
+    #Manages the audio input and language comprehensions
+    #Manages audio output of sound
+    #locates direction of sound
+    pass
